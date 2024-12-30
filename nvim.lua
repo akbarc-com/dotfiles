b258cbb63bded9fbde165eb7552ff1daad61867a
adam <adamakbarc@gmail.com>
2024-12-30 21:58:17 +0700

    yo wagwan bro

--- nvim.lua
+++ nvim.lua
@@ -1,11 +1,4 @@
 local vim = vim
-local Plug = vim.fn['plug#']
-
-vim.call('plug#begin')
-Plug('nvim-lua/plenary.nvim')
-Plug('nvim-telescope/telescope.nvim', { ['branch'] = '0.1.x' })
-Plug('mg979/vim-visual-multi', { ['branch'] = 'master' })
-vim.call('plug#end')
 
 vim.g.mapleader = ' '
 
@@ -21,25 +14,3 @@ vim.opt.tabstop = 2
 vim.opt.clipboard = "unnamedplus"
 vim.keymap.set('i', '<c-h>', '<Esc>', { noremap = true })
 
-vim.api.nvim_create_autocmd("InsertCharPre", {
-  pattern = "*",
-  callback = function()
-    vim.opt.autoindent = true
-  end,
-})
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find Files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find Buffers' })
-vim.keymap.set('n', '<leader>qe', ':bnext<cr>', { desc = 'Next Buffers' })
-vim.keymap.set('n', '<leader>qw', ':bprevious<cr>', { desc = 'Previous Buffers' })
-
-require('telescope').setup {
-  defaults = {
-    file_ignore_patterns = {
-      "node_modules",
-      "vendor",
-    }
-  }
-}
